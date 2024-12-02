76630de875fab91ed90487f436cf14fbd72025b5
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-12-03 02:19:22 +0700

    Fix

--- config/storage.js
+++ config/storage.js
@@ -1,7 +1,7 @@
 const { Storage } = require('@google-cloud/storage');
 require('dotenv').config();
 
-const keyFilename = "../polafit-webservice.json";
+const keyFilename = "../polafit-443507-e984791db3ea.json";
 const projectId = process.env.PROJECT_ID;
 const bucketName = process.env.BUCKET_NAME;  
 
