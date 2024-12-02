fcaf8f5004e67bdfbc054c7eca312dc848743427
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-12-03 01:56:52 +0700

    Fix : Path

--- config/storage.js
+++ config/storage.js
@@ -1,10 +1,11 @@
 const { Storage } = require('@google-cloud/storage');
 require('dotenv').config();
 
-const keyFilename = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const keyFilename = "../polafit-webservice.json";
+const projectId = process.env.PROJECT_ID;
 const bucketName = process.env.BUCKET_NAME;  
 
-const storage = new Storage({ keyFilename });
+const storage = new Storage({ keyFilename, projectId });
 const bucket = storage.bucket(bucketName);
 
 module.exports = { bucket };
