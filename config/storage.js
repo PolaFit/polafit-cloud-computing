c9d84c192a2615e497d59a98bce5b7613eac3d50
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-12-02 23:48:02 +0700

    Fix : Config storage

--- config/storage.js
+++ config/storage.js
@@ -1,7 +1,7 @@
 const { Storage } = require('@google-cloud/storage');
 const path = require('path');
 
-const keyFilename = path.join(__dirname, '/polafit-webservice.json');
+const keyFilename = path.join(__dirname, 'polafit-webservice.json');
 
 const bucketName = 'polafit-bucket';
 
